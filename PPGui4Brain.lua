9d718e6597ea250cf993ac52032bde4d0a4dd865
Mestalic <87284221+Mestalic@users.noreply.github.com>
2022-04-29 16:20:30 +0200

    Update PPGui4Brain.lua

--- PPGui4Brain.lua
+++ PPGui4Brain.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/TheRealKazuma/Gay/main/MestalicPPGuiV2.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TheRealKazuma/Gay/main/PpGui.lua", true))()
