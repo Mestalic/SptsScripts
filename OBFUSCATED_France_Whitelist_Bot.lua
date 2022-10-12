19b04d05b1a000b3bbdaec9c7606723cbffbda9e
Mestalic <87284221+Mestalic@users.noreply.github.com>
2022-10-13 00:06:20 +0200

    Update OBFUSCATED_France_Whitelist_Bot.lua

--- OBFUSCATED_France_Whitelist_Bot.lua
+++ OBFUSCATED_France_Whitelist_Bot.lua
@@ -1,5 +1,5 @@
 _G.LengthOfFlinging = 61
-_G.ChatMessage = 'Add me losers😴😴😴 Mestalic2007'
+_G.ChatMessage = 'Join you pgussies .gg/WZbHK7yPHX😴😴😴 I Mestalic2007'
 _G.Whitelist = {
     508960114,
     2053921341,
